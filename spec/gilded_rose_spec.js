068086acfb06c7ab511ed356c51e669ddb15a942
Unai Recio <unai.recio@agcpartners.co.uk>
2015-08-13 19:59:02 +0100

    test(gilded-rose): 70%

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,44 @@
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
-    update_quality();
+  describe('Item', function () {
+    it('should have a sell_in value', function () {
+      items.forEach(function(item){
+        expect(item.sell_in).toBeDefined();
+      });
+    });
+    it('should have a quality value', function () {
+      items.forEach(function(item){
+        expect(item.quality).toBeDefined();
+      });
+    });
   });
+  describe('None cheese items', function () {
+    describe('the end of each day', function () {
+      var initialItemsOnTheDay = [];
+      beforeEach(function () {
+        items.forEach(function(item) {
+          initialItemsOnTheDay.push(
+            {sell_in: item.sell_in,
+            quality: item.quality}
+          );
+        });
+        update_quality();
+      });
+      fit('should decrease the quality by 1', function () {
+
+        items.forEach(function(item, index) {
+          expect(item.quality).toBe(initialItemsOnTheDay[index].quality - 1);
+        });
+
+      });
+      it('should decrease the sell_in by 1', function () {
+        items.forEach(function(item, index){
+          expect(item.quality).toBe(initialItemsOnTheDay[index].sell_in - 1);
+        });
+      });
+    });
+  });
+
+
 
 });
